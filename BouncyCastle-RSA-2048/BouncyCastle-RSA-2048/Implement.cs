38cc49f164906f6c5f4739d5c10eae66e3fcd18e
Pranav Singh <pranav.horror@gmail.com>
2020-12-03 10:53:31 +0530

    Added RSA2048 calc sign and verify project

---
+++ BouncyCastle-RSA-2048/BouncyCastle-RSA-2048/Implement.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Text;
+using Org.BouncyCastle.Crypto;
+using Org.BouncyCastle.Crypto.Generators;
+using Org.BouncyCastle.Crypto.Parameters;
+using Org.BouncyCastle.Security;
+using Org.BouncyCastle.Asn1.Pkcs;
+
+namespace BouncyCastle_Implement
+{
+    public class Implement
+    {
+        readonly RsaKeyParameters PrivKey;
+        readonly RsaKeyParameters PubKey;
+        byte[] Msg, signature;
+
+        public Implement()
+        {
+            RsaKeyPairGenerator RsaKeyPairGen = new RsaKeyPairGenerator();
+            RsaKeyPairGen.Init(new KeyGenerationParameters(new SecureRandom(), 2048));
+            AsymmetricCipherKeyPair KeyPair = RsaKeyPairGen.GenerateKeyPair();
+
+            PrivKey = (RsaKeyParameters)KeyPair.Private;
+            PubKey = (RsaKeyParameters)KeyPair.Public;
+        }
+
+        public string Sign(string msg)
+        {
+            Msg = Encoding.UTF8.GetBytes(msg);
+
+            ISigner sign = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+            sign.Init(true, PrivKey);
+            sign.BlockUpdate(Msg, 0, Msg.Length);
+            signature = sign.GenerateSignature();
+
+            return BitConverter.ToString(signature);
+        }
+
+        public bool Verify(string msg)
+        {
+            Msg = Encoding.UTF8.GetBytes(msg);
+
+            ISigner VerifySign = SignerUtilities.GetSigner(PkcsObjectIdentifiers.Sha256WithRsaEncryption.Id);
+            VerifySign.Init(false, PubKey);
+            VerifySign.BlockUpdate(Msg, 0, Msg.Length);
+            bool stat = VerifySign.VerifySignature(signature);
+
+            return stat;
+        }
+    }
+}
---
+++ BouncyCastle-RSA-2048/Sign_Verify/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using BouncyCastle_Implement;
+
+namespace Sign_Verify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Msg, Signature;
+            bool Verify;
+            Implement Stream = new Implement();
+
+            Console.WriteLine("Enter message to Sign");
+            Msg = Console.ReadLine();
+            Signature = Stream.Sign(Msg);
+            Console.WriteLine();
+            Console.WriteLine("The Generated Signature is:");
+            Console.WriteLine(Signature);
+            Console.WriteLine();
+
+            Console.WriteLine("Enter message to Verify Signature");
+            Msg = Console.ReadLine();
+            Verify = Stream.Verify(Msg);
+
+            switch (Verify)
+            {
+                case true:
+                    Console.WriteLine("************************");
+                    Console.WriteLine("*  Signature is valid  *");
+                    Console.WriteLine("************************");
+                    break;
+
+                case false:
+                    Console.WriteLine("****************************");
+                    Console.WriteLine("*  Signature is not valid  *");
+                    Console.WriteLine("****************************");
+                    break;
+            }
+        }
+    }
+}
